249ccd2366e727b432e27e2a290270128ca8014b
ahmad2009qqq <ahmad2009.q@gmail.com>
2024-12-27 15:11:35 +0700

    Initial commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,282 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_medical_app/widgets/specialist_item.dart';
+import 'package:flutter_medical_app/widgets/doctor_item.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        iconSize: 24,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home_outlined,
+              color: Colors.black54,
+            ),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.calendar_month_outlined,
+              color: Colors.black54,
+            ),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.chat_bubble_outline,
+              color: Colors.black54,
+            ),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.notifications_none_outlined,
+              color: Colors.black54,
+            ),
+            label: '',
+          ),
+        ],
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 12,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text(
+                          "Hello,",
+                          style: TextStyle(
+                            color: Colors.black54,
+                            fontSize: 16,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 4,
+                        ),
+                        Text(
+                          "dr. Rafki Haris",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const CircleAvatar(
+                      radius: 30,
+                      backgroundImage: AssetImage("assets/drRafkiHaris.jpg"),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 16,
+                    vertical: 16,
+                  ),
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 223, 200, 228),
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Image.asset(
+                        "assets/surgeon.png",
+                        width: 92,
+                        height: 100,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            "How do you feel?",
+                            style: TextStyle(
+                              color: Colors.black87,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 4,
+                          ),
+                          const SizedBox(
+                            width: 120,
+                            child: Text(
+                              "Fill out your medical right now",
+                              style: TextStyle(
+                                color: Colors.black87,
+                                fontSize: 12,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Container(
+                            width: 150,
+                            height: 35,
+                            padding: const EdgeInsets.all(8.0),
+                            decoration: BoxDecoration(
+                              color: Colors.blueAccent,
+                              borderRadius: BorderRadius.circular(12.0),
+                            ),
+                            child: const Center(
+                              child: Text(
+                                "Get Started",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  padding: const EdgeInsets.only(left: 16),
+                  height: 64,
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(95, 179, 173, 173),
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  child: Row(
+                    children: const [
+                      Icon(
+                        Icons.search,
+                        size: 32,
+                        color: Colors.black54,
+                      ),
+                      SizedBox(
+                        width: 12,
+                      ),
+                      Text(
+                        "How can we help you?",
+                        style: TextStyle(
+                          color: Colors.black54,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                  height: 60,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: const [
+                      SpecialistItem(
+                        imagePath: "assets/psychotherapy.png",
+                        imageName: "Psycology",
+                      ),
+                      SizedBox(
+                        width: 8,
+                      ),
+                      SpecialistItem(
+                          imagePath: "assets/surgeon.png",
+                          imageName: "Surgeon"),
+                      SizedBox(
+                        width: 8,
+                      ),
+                      SpecialistItem(
+                          imagePath: "assets/physical_therapy.png",
+                          imageName: "Therapy"),
+                      SizedBox(
+                        width: 8,
+                      ),
+                      SpecialistItem(
+                          imagePath: "assets/dentist_skin.png",
+                          imageName: "Dentist"),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    Text(
+                      "Doctor list",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                    Text(
+                      "see all",
+                      style: TextStyle(
+                        color: Colors.black45,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                  height: 200,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: const [
+                      DoctorItem(
+                          image: "assets/drRafkiHaris.jpg",
+                          name: "Rafki Haris",
+                          speciaList: "Pediatrician"),
+                      DoctorItem(
+                          image: "assets/drSarahGardie.jpg",
+                          name: "Sarah Gardie",
+                          speciaList: "Surgeon"),
+                      DoctorItem(
+                          image: "assets/drTarunaIkrar.jpg",
+                          name: "Taruna Ikrar",
+                          speciaList: "Odontologist"),
+                      DoctorItem(
+                          image: "assets/drNadyaAlaydrus.jpg",
+                          name: "Nadya A.",
+                          speciaList: "Ophtamologist"),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/doctor_item.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class DoctorItem extends StatelessWidget {
+  final String image;
+  final String name;
+  final String speciaList;
+  const DoctorItem(
+      {Key? key,
+      required this.image,
+      required this.name,
+      required this.speciaList})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: Container(
+        width: 160,
+        decoration: BoxDecoration(
+            color: const Color.fromARGB(95, 179, 173, 173),
+            borderRadius: BorderRadius.circular(15.0)),
+        padding: const EdgeInsetsDirectional.symmetric(
+            horizontal: 14.0, vertical: 20.0),
+        // padding: const EdgeInsets.symmetric(horizontal: 14.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            // const SizedBox(
+            //   height: 20,
+            // ),
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage(image),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              "dr. $name",
+              overflow: TextOverflow.fade,
+              maxLines: 2,
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            Text(
+              speciaList,
+              textAlign: TextAlign.center,
+              style: const TextStyle(color: Colors.black54),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
